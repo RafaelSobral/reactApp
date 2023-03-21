71e2d607f9f4d8dbc106afa72967526a62b9e0e9
Rafael Sobral <rafaelsobral@outlook.com>
2023-03-20 21:38:39 -0300

    correção_routes

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,13 +1,12 @@
-import { BrowserRouter, Navigate, Route } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { Dashboard } from "../pages/dashboard/Dashboard"
 
-export const Routes = () => {
+export const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/pagina-inicial" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/pagina-inicial" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
   )
 }
+export { Routes }
